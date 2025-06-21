6cd2eea2f274d80cf7b5cf6aa32d377c9bcaa1ab
pidokige02 <pidokige0204@gmail.com>
2025-06-21 23:17:03 +0900

    support BibleList in admin

--- bibleview/admin.py
+++ bibleview/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BibleVerse
+
+@admin.register(BibleVerse)
+class BibleVerseAdmin(admin.ModelAdmin):
+    list_display = ('version', 'book', 'chapter', 'verse', 'short_text')
+    list_filter = ('version', 'book', 'chapter')
+    search_fields = ('book', 'text')
+
+    def short_text(self, obj):
+        return obj.text[:50] + "..."
+    short_text.short_description = 'Text'
