a9cd5ef8808601ddbe5750a291aa8b867165a6b3
pidokige02 <pidokige0204@gmail.com>
2025-10-07 10:34:28 +0900

    adding view for kakao map

--- kakao/views.py
+++ kakao/views.py
@@ -3,6 +3,7 @@ import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from django.shortcuts import render
 
 @csrf_exempt  # 개발 중에는 편의상 사용, 배포 전에는 주의
 def book_search(request):
@@ -31,3 +32,13 @@ def book_search(request):
     except requests.exceptions.RequestException as e:
         return JsonResponse({'error': str(e)}, status=500)
 # url 에 header 와 param 을 넘기는 방법이 나와 있고, 결과를 json 방식으로 client 에  넘기고 있다.
+
+def kakao_map(request):
+    """
+    Kakao JavaScript SDK를 이용한 지도 페이지
+    (AppKey는 서버에서 안전하게 주입)
+    """
+    context = {
+        "kakao_key": settings.KAKAO_JS_KEY,
+    }
+    return render(request, "kakao/map.html", context)
